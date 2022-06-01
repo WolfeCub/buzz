001da8df89c173734f4c501ce05172037412e5bb
WolfeCub <1369773+WolfeCub@users.noreply.github.com>
2022-06-01 18:35:18 +0000

    Deploying to gh-pages from @ WolfeCub/buzz@7c491a0079c2bfbe830a2ef10e84d4992fd4d872 🚀

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["buzz","buzz_codegen","buzz_examples","buzz_types"];
+window.ALL_CRATES = ["buzz","buzz_codegen","buzz_types"];
--- source-files.js
+++ source-files.js
@@ -1,6 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["buzz"] = {"name":"","dirs":[{"name":"http_parse","files":["mod.rs","parser.rs"]}],"files":["buzz.rs","lib.rs"]};
 sourcesIndex["buzz_codegen"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["buzz_examples"] = {"name":"","files":["main.rs"]};
 sourcesIndex["buzz_types"] = {"name":"","dirs":[{"name":"traits","files":["mod.rs","respond.rs"]}],"files":["http_method.rs","http_response.rs","lib.rs","route_metadata.rs"]};
 createSourceSidebar();
