303251ac9e719ae1f490a2982594bab59ed507b8
Josh Wolfe <joshuafwolfe@gmail.com>
2022-06-27 19:13:20 -0400

    Add docs for Inject and InjectMut

--- buzz-types/src/inject.rs
+++ buzz-types/src/inject.rs
@@ -1,5 +1,16 @@
 use std::ops::{Deref, DerefMut};
 
+/// Wrapper that indicates a shared reference should be injected.
+///
+/// Multiple functions can have exclusive references to the same type.
+/// You can think of this as `&T`.
+///
+/// ```ignore
+/// #[get("/inject")]
+/// fn inject(val: Inject<i32>) -> impl Respond {
+///     val.to_string()
+/// }
+/// ```
 pub struct Inject<'a, T> {
     inner: &'a T,
 }
@@ -24,6 +35,18 @@ impl<'a, T> Deref for Inject<'a, T> {
     }
 }
 
+/// Wrapper that indicates an exclusive reference should be injected.
+///
+/// Only one function may have a mutable reference to this value.
+/// You can think of this as `&mut T`. Any calls that try to get a [`InjectMut`] will block until
+/// it's available.
+///
+/// ```ignore
+/// #[get("/inject-mut")]
+/// fn inject_mut(mut val: InjectMut<i32>) -> impl Respond {
+///     *val = 77;
+/// }
+/// ```
 pub struct InjectMut<'a, T> {
     inner: &'a mut T,
 }
