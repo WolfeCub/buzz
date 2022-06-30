24c9f19d47f2ac4db1ab7497179c4f8b35aa4819
WolfeCub <1369773+WolfeCub@users.noreply.github.com>
2022-06-30 02:48:41 +0000

    Deploying to gh-pages from @ WolfeCub/buzz@894b27358913c9724a5b1e3e5e009a525091e651 🚀

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["buzz"] = {"name":"","dirs":[{"name":"http_parse","files":["mod.rs"]},{"name":"json","dirs":[{"name":"parse","files":["json_value.rs","mod.rs","tokenizer.rs"]}],"files":["json.rs","mod.rs"]}],"files":["buzz.rs","lib.rs","routes.rs"]};
 sourcesIndex["buzz_codegen"] = {"name":"","files":["lib.rs","route_parser.rs","routes.rs","utils.rs"]};
-sourcesIndex["buzz_types"] = {"name":"","dirs":[{"name":"traits","files":["deserialize.rs","from_body.rs","mod.rs","respond.rs"]}],"files":["buzz_context.rs","dependancy_injection.rs","errors.rs","handler.rs","http_method.rs","http_request.rs","http_response.rs","inject.rs","lib.rs","parser.rs","route.rs","route_metadata.rs"]};
+sourcesIndex["buzz_types"] = {"name":"","dirs":[{"name":"traits","files":["deserialize.rs","from_body.rs","mod.rs","respond.rs"]}],"files":["buzz_context.rs","dependancy_injection.rs","errors.rs","handler.rs","headers.rs","http_method.rs","http_request.rs","http_response.rs","inject.rs","lib.rs","parser.rs","route.rs","route_metadata.rs"]};
 createSourceSidebar();
