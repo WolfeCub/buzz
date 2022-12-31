ade16c1f32941046697ea372ad36f8eec09ec223
WolfeCub <1369773+WolfeCub@users.noreply.github.com>
2022-12-31 03:52:11 +0000

    Deploying to gh-pages from @ WolfeCub/buzz@cd29d4e565c40256a45d630eccd26b3005de6a59 🚀

--- implementors/core/ops/deref/trait.DerefMut.js
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"buzz_types":[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"buzz_types/struct.InjectMut.html\" title=\"struct buzz_types::InjectMut\">InjectMut</a>&lt;'a, T&gt;"]]
+"buzz_types":[["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"buzz_types/struct.InjectMut.html\" title=\"struct buzz_types::InjectMut\">InjectMut</a>&lt;'a, T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
